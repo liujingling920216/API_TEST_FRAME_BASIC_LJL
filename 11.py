a3d95da42f26e3eeeaf5287aabcb9a4c450e061d
sophia <1040616989@qq.com>
2020-07-10 23:00:42 +0800

    demo修改

--- 11.py
+++ 11.py
@@ -11,14 +11,16 @@ token = re.findall('" name="csrf_token" value="(.+?)"/', rep01)[0]
 print(token)
 
 # 注册
-url02 = 'http://47.107.178.45/phpwind/index.php?m=u&c=register'
-get_param = {
+url02 = 'http://47.107.178.45/phpwind/index.php'
+get_params={}
+
+post_data = {
     "username":"sophia02",
     "password":"123456",
     "repassword":"123456",
     "email":"test111@cardinfolink.com",
     "csrf_token":token
 }
-rep02 = session.post(url=url02,data=get_param)
+rep02 = session.post(url=url02,data=post_data,params= )
 print(rep02.content.decode("utf-8"))
 print(rep02.status_code)
