b9ab82dac6864e0fff3c51c9046831bbdb9aa04e
sophia <sophia.liu@cardinfolink.com>
2020-07-10 18:52:30 +0800

    线性接口测试框架优化-模块化思想

---
+++ 11.py
@@ -0,0 +1,24 @@
+import re
+import requests
+
+session = requests.session()
+
+# 授权页面
+url01 = 'http://47.107.178.45' + '/phpwind/'
+res01 = session.get(url=url01)
+rep01 = res01.content.decode('utf-8')
+token = re.findall('" name="csrf_token" value="(.+?)"/', rep01)[0]
+print(token)
+
+# 注册
+url02 = 'http://47.107.178.45/phpwind/index.php?m=u&c=register'
+get_param = {
+    "username":"sophia02",
+    "password":"123456",
+    "repassword":"123456",
+    "email":"test111@cardinfolink.com",
+    "csrf_token":token
+}
+rep02 = session.post(url=url02,data=get_param)
+print(rep02.content.decode("utf-8"))
+print(rep02.status_code)
